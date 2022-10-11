826b1395030cf485bc6df0e108c40b5ac4f9cecb
Henrique Moreira <riqimoreiraa@gmail.com>
2022-10-11 10:40:15 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router);
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}...`);
