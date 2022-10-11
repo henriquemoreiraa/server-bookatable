c1b994a526a7c98e50a67648342a65fe8488f52a
Henrique Moreira <riqimoreiraa@gmail.com>
2022-10-11 10:47:47 -0300

    v

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router);
-app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}...`);
 });
 
 export { app };
-
-//index.js
